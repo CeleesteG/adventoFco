b6d2af4704996cb3417c720bc91da87343d0f3ce
Celeste <celestegarciacarrillo@gmail.com>
2024-12-04 21:44:42 -0600

    dia3

---
+++ day3/dia3-part2.js
@@ -0,0 +1,27 @@
+//PENDIENTE BYE
+let text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+const regexp= new RegExp(/mul\(\d{1,3},\d{1,3}\)/gm);
+
+const myArray = [...text.matchAll(regexp)];
+
+//console.log ('result ' + myArray);
+
+let sum = 0;
+myArray.map((element) => {
+    const reN1 = /\d{1,3},/;
+    const reN2 = /,\d{1,3}/;
+    let mult = 0;
+  
+    num1 =  (element.toString().match(reN1)).toString().replace(',', '');
+    num2 = (element.toString().match(reN2)).toString().replace(',', '');
+    mult = num1 * num2;
+   // console.log ('mul ' + mult);
+
+    sum+= mult;
+   
+});
+console.log ('sum ' + sum);
+
+
+
